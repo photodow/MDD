365a5af42947b79fe5461b6602f1b4d5f22a3a44
James <photodow@gmail.com>
2014-02-19 02:29:01 -0500

    [#65848474][#65848486] added one global variable and login control
    Added one global variable so certain methods and data can be access
    throughout the whole app without having to worry about global memory
    leaks.

--- js/app.js
+++ js/app.js
@@ -1,58 +1,137 @@
-function authStatus(authorized, notAuthorized) {
+var app = {
 
-    'use strict';
+    root: '/MDD/#',
 
-    if (IN.User.isAuthorized()) {
-        authorized();
-    } else {
-        notAuthorized();
-    }
+    inOnLoad: function (doThis) {
 
-}
+        'use strict';
 
-function LinkedIn() {
+        IN.Event.on(IN, 'frameworkLoaded', function(){
 
-    'use strict';
+            doThis();
 
-    (function () {
-
-        var linkedInLogin;
-
-        linkedInLogin = $('#linkedInLogin');
-
-        function toggleBtn() {
-
-            var loginBtn, loginHTML, logoutHTML;
-
-            loginHTML = 'Sign in with LinkedIn <i class="fa fa-arrow-circle-o-right"></i>';
-            logoutHTML = 'Logout of LinkedIn <i class="fa fa-sign-out"></i>';
-            loginBtn = linkedInLogin.find('button');
-
-            authStatus(function () {
-                loginBtn.html(logoutHTML);
-            }, function () {
-                loginBtn.html(loginHTML);
-            });
-
-        }
-
-        linkedInLogin.on('mousedown', 'button', function () {
-
-            authStatus(function () {
-                IN.User.logout(function () {
-                    toggleBtn();
-                });
-            }, function () {
-                IN.User.authorize(function () {
-                    toggleBtn();
-                });
-            });
-
-            return false;
         });
 
-        toggleBtn();
+    },
 
-    }());
+    checkAuth: function (param1, param2) {
 
-}
+        'use strict';
+
+        app.inOnLoad(function(){
+
+            if (typeof param1 === 'function') {
+
+                var authStatus, authTrue, authFalse;
+
+                authStatus = IN.User.isAuthorized();
+                authTrue = param1;
+
+                if (typeof param2 === 'function') {
+
+                    authFalse = param2;
+
+                    if (authStatus) {
+
+                        authTrue();
+
+                    } else {
+
+                        authFalse();
+
+                    }
+
+                } else if (String(param2) === 'false') {
+
+                    authFalse = param1;
+
+                    if (!authStatus) {
+
+                        authFalse();
+
+                    }
+
+                } else {
+
+                    if (authStatus) {
+
+                        authTrue();
+
+                    }
+
+                }
+
+            }
+
+        });
+
+    },
+
+    inOnLogin: function (doThis) {
+
+        IN.Event.on(IN, 'auth', function(){
+
+            doThis();
+
+        });
+
+    },
+
+    inOnLogout: function (doThis) {
+
+        IN.Event.on(IN, 'logout', function(){
+
+            doThis();
+
+        });
+
+    }
+
+};
+
+
+
+
+
+app.inOnLoad(function(){
+
+    'use strict';
+
+    var linkedInLogin;
+
+    linkedInLogin = $('#linkedInLogin');
+
+    function toggleBtn() {
+
+        var loginBtn, loginHTML, logoutHTML;
+
+        loginHTML = 'Sign in with LinkedIn <i class="fa fa-arrow-circle-o-right"></i>';
+        logoutHTML = 'Logout of LinkedIn <i class="fa fa-sign-out"></i>';
+        loginBtn = linkedInLogin.find('button');
+
+        app.checkAuth(function () {
+            loginBtn.html(logoutHTML);
+        }, function () {
+            loginBtn.html(loginHTML);
+        });
+
+    }
+
+    linkedInLogin.on('mousedown', 'button', function () {
+
+        app.checkAuth(function () {
+            IN.User.logout(function () {
+                toggleBtn();
+            });
+        }, function () {
+            IN.User.authorize(function () {
+                toggleBtn();
+            });
+        });
+
+        return false;
+    });
+
+    toggleBtn();
+
+});
